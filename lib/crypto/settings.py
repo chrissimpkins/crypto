9a90859ef5b794a5b9e8caa2aa2b2e9338c6dfb6
Chris Simpkins <git.simpkins@gmail.com>
2014-11-09 16:56:37 -0500

    updated settings.py

--- lib/crypto/settings.py
+++ lib/crypto/settings.py
@@ -29,7 +29,7 @@ Encrypt by explicit file path:
 
 Encrypt by directory path:
 --------------------------
-  crypto [directory path]
+  crypto [directory path] <directory path>
 
 """
 
@@ -37,25 +37,26 @@ Encrypt by directory path:
 # Help String
 #------------------------------------------------------------------------------
 help = """
--------------------------------------
+---------------------------------------
 crypto
 Simple symmetric GPG file encryption
 Copyright 2014 Christopher Simpkins
 MIT license
--------------------------------------
+https://github.com/chrissimpkins/crypto
+---------------------------------------
 
 ABOUT
-  crypto provides a simple interface to the Gnu Privacy Guard (gpg) application for symmetric encryption of one or more files.  gpg must be installed on your system in order to use crypto.
+  crypto provides a simple interface to Gnu Privacy Guard (gpg) encryption for one or more files.  gpg must be installed on your system in order to use crypto.
 
 USAGE
   crypto [file path] <file path>
-  crypto [directory path]
+  crypto [directory path] <directory path>
 
 OPTIONS
-   --armor        Use a portable ASCII armored format
+   --armor | -a       Use a portable ASCII armored encryption format
 
 DESCRIPTION
-  Use one or more explicit file paths to encrypt the file(s).  Use a directory argument to encrypt all files in the top level of the directory with the same passphrase.  The encrypted file is named '<filename>.crypt' and will be located in the same directory as the original file.  crypto does not remove or otherwise modify the original file.
+  Use one or more explicit file paths to encrypt the file(s).  Use one or more directory arguments to encrypt all files in the top level of the directory with the same passphrase.  Encrypted files are named '<filename>.crypt' and will be located in the same directory as the original file.  crypto does not remove or otherwise modify the original file.
 
-  Encryption is performed with the AES256 cipher.
+  Encryption is performed with the AES256 cipher algorithm.
 """
