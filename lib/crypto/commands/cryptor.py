48b45094732f58d638e641e1c76ecfa4a5b40a4f
Chris Simpkins <git.simpkins@gmail.com>
2014-12-03 14:19:19 -0500

    stubs for the Cryptor and Decryptor classes

--- lib/crypto/commands/cryptor.py
+++ lib/crypto/commands/cryptor.py
@@ -1,12 +1,46 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-class Cryptor:
-    def __init__(self):
+#------------------------------------------------------------------------------
+# Cryptor class
+#   performs gpg encryption of one or more files
+#------------------------------------------------------------------------------
+class Cryptor(object):
+    def __init__(self, passphrase):
+        self.command-default = "gpg --batch --force-mdc --cipher-algo AES256 -o "
+        self.command-nocompress = "gpg -z 0 --batch --force-mdc --cipher-algo AES256 -o "
+        self.command-maxcompress = "gpg -z 9 --batch --force-mdc --cipher-algo AES256 -o "
+
+    def encrypt_file(self, file_path):
+        pass
+
+    def encrypt_files(self, file_list):
+        pass
+
+    def encrypt_directory(self, dir_path):
+        pass
+
+    def encrypt_directories(self, dir_list):
         pass
 
 
-class Decryptor:
+
+#------------------------------------------------------------------------------
+# Decryptor class
+#   performs gpg decryption of one or more files
+#------------------------------------------------------------------------------
+class Decryptor(object):
     def __init__(self):
         pass
 
+    def decrypt_file(self, file_path):
+        pass
+
+    def decrypt_files(self, file_list):
+        pass
+
+    def decrypt_directory(self, dir_path):
+        pass
+
+    def decrypt_directories(self, dir_list):
+        pass
