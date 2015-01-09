9729fb00a9fd6ade2a8d1fd48fcc7daef7d0979c
Chris Simpkins <git.simpkins@gmail.com>
2015-01-09 18:38:30 -0500

    new methods + updated methods for file compression associated speedups

--- lib/crypto/library/cryptor.py
+++ lib/crypto/library/cryptor.py
@@ -19,6 +19,8 @@ class Cryptor(object):
         self.command_nocompress_armored = "gpg -z 0 --armor --batch --force-mdc --cipher-algo AES256 -o "
         self.command_maxcompress_armored = "gpg -z 7 --armor --batch --force-mdc --cipher-algo AES256 -o "
         self.passphrase = passphrase
+        self.common_binaries = set(['.7z', '.gz', '.app', '.avi', '.bz2', '.deb', '.doc', '.dmg', '.exe', '.flv', '.gif', '.jar', '.jpg', '.mov', '.mp3', '.mp4', '.odt', '.ogg', '.pdf', '.pkg', '.png', '.ppt', '.pps', '.psd', '.rar', '.rpm', '.tar', '.tif', '.wav', '.wmv', '.xls', '.zip', '.docx', '.epub', '.flac', '.mpeg', '.jpeg', '.pptx', '.xlsx'])
+        self.common_text = set(['.c', '.h', '.m', '.cc', '.js', '.pl', '.py', '.rb', '.sh', '.cpp', '.css', '.csv', '.php', '.rss', '.txt', '.xml', '.yml', '.java', '.json', '.html', '.yaml'])
 
     #------------------------------------------------------------------------------
     # PUBLIC methods
@@ -97,18 +99,70 @@ class Cryptor(object):
         """private method that generates the crypto saved file path string with a .crypt file type"""
         return inpath + '.crypt'
 
+
     def _is_compress_filetype(self, inpath):
         """private method that performs magic number and size check on file to determine whether to compress the file"""
-        # files > 10kB get checked for compression
-        if file_size(inpath) > 10240:
-            try:
-                response = muterun("file --mime-type -b " + inpath)
-                if response.stdout[0:5] == "text/": # check for a text file mime type
-                    return True  # appropriate size, appropriate file mime type
-                else:
-                    return False # appropriate size, inappropriate file mime type
-            except Exception:
-                return False
+        # check for common file type suffixes in order to avoid the need for file reads to check magic number for binary vs. text file
+        if self._is_common_binary(inpath):
+            return False
+        elif self._is_common_text(inpath):
+            return True
         else:
-            return False # inappropriate size, skip mime type check
+            # files > 10kB get checked for compression (arbitrary decision to skip compression on small files)
+            the_file_size = file_size(inpath)
+            if the_file_size > 10240:
+                if the_file_size > 512000:  # seems to be a break point at ~ 500kb where file compression offset by additional file read, so limit tests to files > 500kB
+                    try:
+                        response = muterun("file --mime-type -b " + inpath)
+                        if response.stdout[0:5] == "text/": # check for a text file mime type
+                            return True  # appropriate size, appropriate file mime type
+                        else:
+                            return False # appropriate size, inappropriate file mime type
+                    except Exception:
+                        return False
+                else:
+                    return True  # if file size is < 500kB, skip the additional file read and just go with compression
+            else:
+                return False # below minimum size to consider compression, do not compress
+        
+        
+    def _is_common_binary(self, inpath):
+        """compare file path mime type to common binary file types"""
+        # make local variables for the available char numbers in the suffix types to be tested
+        two_suffix = inpath[-3:]
+        three_suffix = inpath[-4:]
+        four_suffix = inpath[-5:]
+        
+        # test for inclusion in the instance variable common_binaries (defined in __init__)
+        if two_suffix in self.common_binaries:
+            return True
+        elif three_suffix in self.common_binaries:
+            return True
+        elif four_suffix in self.common_binaries:
+            return True
+        else:
+            return False
+        
+        
+    def _is_common_text(self, inpath):
+        """compare file path mime type to common text file types"""
+        # make local variables for the available char numbers in the suffix types to be tested
+        one_suffix = inpath[-2:]
+        two_suffix = inpath[-3:]
+        three_suffix = inpath[-4:]
+        four_suffix = inpath[-5:]
+        
+        # test for inclusion in the instance variable common_text (defined in __init__)
+        if one_suffix in self.common_text:
+            return True
+        elif two_suffix in self.common_text:
+            return True
+        elif three_suffix in self.common_text:
+            return True
+        elif four_suffix in self.common_text:
+            return True
+        else:
+            return False
+        
+        
 
