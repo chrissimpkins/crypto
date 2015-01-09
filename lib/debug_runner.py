3db95215eb62ddabe5e36d5d48fd45a4adc5d9a8
Chris Simpkins <git.simpkins@gmail.com>
2015-01-09 18:37:19 -0500

    commented out debug tests

---
+++ lib/debug_runner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# from crypto.library.cryptor import Cryptor
+
+# pdf_file = "test.pdf"
+# c = Cryptor("test")
+# response = c._is_common_binary(pdf_file)
