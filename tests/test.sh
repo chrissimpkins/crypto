788c43e6cc2b4035aea90fdd9e3145d445e92110
Chris Russ <chruss@gmx.de>
2015-05-12 15:55:05 +0200

    adding the test-case

--- tests/test.sh
+++ tests/test.sh
@@ -16,6 +16,7 @@ MULTI_DIR="test_multi-directory.py"
 SINGLE_FILE="test_single-file.py"
 SINGLE_DIR="test_single-directory.py"
 UNICODE_PP="test_unicode-passphrase.py"
+ESCAPE_PP="test_escaping-passphrase.py"
 
 if [ "$1" = "all" ];then
 	"$TEST_COMMAND" "$NOSE_FLAGS" "$SINGLE_FILE" "$SINGLE_DIR"
@@ -43,6 +44,8 @@ elif [ "$1" = "single-dir" ];then
 	"$TEST_COMMAND" "$NOSE_FLAGS" "$SINGLE_DIR"
 elif [ "$1" = "unicode" ];then
 	"$TEST_COMMAND" "$NOSE_FLAGS" "$UNICODE_PP"
+elif [ "$1" = "escape" ];then
+	"$TEST_COMMAND" "$NOSE_FLAGS" "$ESCAPE_PP"
 else
 	echo "Enter 'all' or a command suite to test."
 	exit 1
